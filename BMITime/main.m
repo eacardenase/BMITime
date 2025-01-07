fbb2365570826872b5e9577d44d57d7f2d71d084
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-06 19:41:49 -0500

    feat: use employee specific methods

--- BMITime/main.m
+++ BMITime/main.m
@@ -10,17 +10,21 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        BNRPerson *edwin = [[BNREmployee alloc] init];
+        BNREmployee *edwin = [[BNREmployee alloc] init];
         
         edwin.heightInMeters = 1.8;
         edwin.weightInKilos = 96;
+        edwin.employeeID = 20;
+        edwin.hireDate = [NSDate dateWithNaturalLanguageString:@"Dec 23rd, 2024"];
         
         float height = edwin.heightInMeters;
         int weight = edwin.weightInKilos;
         NSLog(@"edwin is %.2f meters tall and weighs %d kilograms", height, weight);
+        NSLog(@"Employee %u hired on %@", edwin.employeeID, edwin.hireDate);
         
         float bmi = [edwin bodyMassIndex];
-        NSLog(@"edwin has a BMI of %.2f", bmi);
+        double years = [edwin yearsOfEmployment];
+        NSLog(@"BMI of %.2f, has worked with us for %.2f years.", bmi, years);
     }
     return 0;
 }
