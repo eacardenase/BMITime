3468ebf54ea4b9072515fd1152d5000f594fc4e5
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-11 20:18:44 -0500

    feat: arc in action

--- BMITime/main.m
+++ BMITime/main.m
@@ -7,27 +7,45 @@
 
 #import <Foundation/Foundation.h>
 #import "BNREmployee.h"
+#import "BNRAsset.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        BNREmployee *edwin = [[BNREmployee alloc] init];
+        NSMutableArray *employees = [NSMutableArray array];
         
-        edwin.heightInMeters = 1.8;
-        edwin.weightInKilos = 96;
-        edwin.employeeID = 20;
+        for (int i = 0; i < 10; i++) {
+            BNREmployee *edwin = [[BNREmployee alloc] init];
+            
+            edwin.heightInMeters = 1.8 - (i / 10.0);
+            edwin.weightInKilos = 96 + i;
+            edwin.employeeID = i;
+            
+            [employees addObject:edwin];
+        }
         
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.dateFormat = @"yyyy-MM-dd";
-        edwin.hireDate = [dateFormatter dateFromString:@"2024-12-23"];
+        for (int i = 0; i < 10; i++) {
+            BNRAsset *asset = [[BNRAsset alloc] init];
+            
+            NSString *currentLabel = [NSString stringWithFormat:@"Laptop %d", i];
+            asset.label = currentLabel;
+            asset.resaleValue = 350 + (i * 17);
+            
+            NSInteger randomIndex = random() % [employees count];
+            
+            BNREmployee *randomEmployee = [employees objectAtIndex:randomIndex];
+            
+            [randomEmployee addAsset:asset];
+        }
         
-        float height = edwin.heightInMeters;
-        int weight = edwin.weightInKilos;
-        NSLog(@"edwin is %.2f meters tall and weighs %d kilograms", height, weight);
-        NSLog(@"%@ hired on %@", edwin, edwin.hireDate);
+        NSLog(@"Employees: %@", employees);
         
-        float bmi = [edwin bodyMassIndex];
-        double years = [edwin yearsOfEmployment];
-        NSLog(@"BMI of %.2f, has worked with us for %.2f years.", bmi, years);
+        NSLog(@"Giving up ownership of one employee.");
+        
+        [employees removeObjectAtIndex:5];
+        
+        NSLog(@"Giving up ownership of arrays.");
+        
+        employees = nil;
     }
     return 0;
 }
